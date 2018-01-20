f61c3358590bdfb0fd03f638cef49046d0248f8a
Alex <apapantonatos@gmail.com>
2018-01-20 16:54:00 +0100

    The out from the Docker daemon is now correctly logged to stdout.

--- cbuild.go
+++ cbuild.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	// "bufio"
 	"context"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -54,6 +54,10 @@ func NewBuilder() *Builder {
 	return b
 }
 
+func (b *Builder) SetRegion(region string) {
+	b.sess.Config.Region = aws.String(region)
+}
+
 func (b *Builder) Cleanup() {
 	os.RemoveAll(b.tmpdir)
 }
@@ -67,14 +71,14 @@ func (b *Builder) registrylogin(registryID string) (string, error) {
 	} //else default regitstry of the account is assumed
 	auth, err := ecr_client.GetAuthorizationToken(&input)
 	if err != nil {
+		log.Printf("RegistryLogin: Error: %s\n", err)
 		return "", err
 	}
-
 	// b.token = *auth.AuthorizationData[0].AuthorizationToken
 	// // fmt.Printf("token: %s\n", ac.token)
 	// b.exp = auth.AuthorizationData[0].ExpiresAt
 	b.registryID = strings.TrimPrefix(*auth.AuthorizationData[0].ProxyEndpoint, "https://")
-	fmt.Printf("b.registryID: %s\n", b.registryID)
+	log.Printf("RegistryLogin: Got authentication token for registry: %s\n", b.registryID)
 
 	data, err := base64.StdEncoding.DecodeString(*auth.AuthorizationData[0].AuthorizationToken)
 	if err != nil {
@@ -100,7 +104,7 @@ func (b *Builder) push() error {
 	//check if repository exists
 	ecr_client := ecr.New(b.sess)
 
-	repositories, err := ecr_client.DescribeRepositories(&ecr.DescribeRepositoriesInput{
+	_, err := ecr_client.DescribeRepositories(&ecr.DescribeRepositoriesInput{
 		RepositoryNames: []*string{&b.imagename},
 	})
 	if err != nil {
@@ -110,10 +114,11 @@ func (b *Builder) push() error {
 					RepositoryName: &b.imagename,
 				})
 				if err != nil {
+					log.Printf("Push: Error: %s\n", err)
 					return err
 				}
 			} else { //fail on all other errors
-				fmt.Printf("describerepos: %v\n%v\n", repositories, err)
+				log.Printf("Push: Error: %s\n", err)
 				return err
 			}
 		}
@@ -132,8 +137,28 @@ func (b *Builder) push() error {
 		panic(err)
 	}
 
-	resp, _ := ioutil.ReadAll(r)
-	fmt.Println(string(resp))
+	d := json.NewDecoder(r)
+	for {
+		var v map[string]interface{}
+		if err = d.Decode(&v); err != nil { //no need to handle errors other than EOF differently
+			break
+		}
+
+		if status, exists := v["status"]; exists {
+			if status != "Pushing" { //omit messages regarding pushing progress from output
+				if id, exists := v["id"]; exists {
+					log.Printf("Push: %s id:%s", status, id)
+				} else {
+					log.Printf("Push: %s", status)
+				}
+			}
+		}
+		if aux, exists := v["aux"]; exists {
+			m := aux.(map[string]interface{})
+			log.Printf("Push: Tag: %s, Digest: %s, Size: %f\n", m["Tag"].(string), m["Digest"].(string), m["Size"].(float64))
+		}
+
+	}
 
 	return nil
 }
@@ -142,14 +167,33 @@ func (b *Builder) buildFromS3(bucket string, filename string) {
 
 }
 
-//TODO: allo specification of fragments for the repo to cater for cases where the build context is a
+//TODO: improve logging of ImageBuild to also handle "aux" JSON objects in the command's output
 //subdirectory within the repo.
 //For details see https://docs.docker.com/engine/reference/commandline/build/#git-repositories
 func (b *Builder) buildFromGithub(repo string, authkey string, imagename string) error {
 	var credentials, repoURL string
 
-	if imagename == "" {
-		return errors.New("imagename can not be empty")
+	if strings.Contains(repo, ".git") != true {
+		return errors.New("invalid repository name (missing .git ending)")
+	}
+
+	if imagename == "" { //if no imagename defined use the repo name
+		if strings.Contains(repo, "#") {
+			components := strings.Split(repo, "#")
+			frgament := components[1]
+			if strings.Contains(frgament, ":") { // if fragment contains subfolder
+				components = strings.Split(frgament, ":")
+				imagename = components[1] //imagename is the subfolder name
+			} else { //fragment doesn't contain subfolder
+				imagename = components[1] //imagename is the branch name
+				//repo = strings.TrimSuffix(components[0], ".git")
+
+			}
+		} else { //repo URL doesn't contain fragment i.e. no "#" in the URL
+			repo_URI := strings.TrimSuffix(repo, ".git")
+			components := strings.Split(repo_URI, "/")
+			imagename = components[len(components)-1] //imagename is the last path component of the URL
+		}
 	}
 
 	if authkey != "" {
@@ -162,15 +206,16 @@ func (b *Builder) buildFromGithub(repo string, authkey string, imagename string)
 			WithDecryption: &decrypt,
 		})
 		if err != nil {
-			panic(err)
+			log.Printf("Build: Error: %s\n", err)
+			return err
 		}
 		byte_str, err := base64.URLEncoding.DecodeString(*key.Parameter.Value)
 		credentials = string(byte_str)
 		// fmt.Printf("%s\n", credentials)
 
-		repoURL = "https://" + credentials + "@" + repo + ".git"
+		repoURL = "https://" + credentials + "@" + repo
 	} else { // assume this is a public URL that doesn't require authentication
-		repoURL = "https://" + repo + ".git"
+		repoURL = "https://" + repo
 	}
 
 	_, err := b.registrylogin("")
@@ -189,31 +234,23 @@ func (b *Builder) buildFromGithub(repo string, authkey string, imagename string)
 	log.Printf("Building %s...\n", tag)
 	response, err := b.docker.ImageBuild(context.Background(), nil, options)
 	if err != nil {
-		panic(err)
+		log.Printf("Push: Error: %s\n", err)
+		return err
 	}
 
-	// type Message struct {
-	// 	stream string, `json:"stream"`
-	// }
-
-	// var m Message
 	d := json.NewDecoder(response.Body)
 	for {
 		var v map[string]interface{}
-		if err = d.Decode(&v); err != nil {
-			log.Println(err)
+		if err = d.Decode(&v); err != nil { //no need to handle errors other than EOF differently
 			break
 		}
-		for k := range v {
-			switch k {
-			case "stream":
-				log.Printf("%s", v["stream"].(string))
-			case "aux":
-				log.Printf("(aux)%s", v["stream"].(string))
-			}
-			//fmt.Printf("%#v\n", v)
+		if status, exists := v["stream"]; exists {
+			log.Printf("Build: %s", status)
 		}
-
+		if aux, exists := v["aux"]; exists {
+			m := aux.(map[string]interface{})
+			log.Printf("Build: ID: %s", m["ID"])
+		}
 	}
 
 	b.imagename = imagename
@@ -257,11 +294,30 @@ func (b *Builder) downloadBuildCtx(bucket string, filename string) {
 	fmt.Printf("%d, %v\n", length, out)
 }
 
+var cname, repo, region, credentials_ssm_key string
+
+func init() {
+	flag.StringVar(&cname, "name", "", "the name of the container image (default value is the last part of the repo name)")
+	flag.StringVar(&repo, "r", "", "the name of the GitHub repository containing the container build files")
+	flag.StringVar(&credentials_ssm_key, "c", "", "the name of an AWS systems manager parameter containing credentials for the GitHub repository. Not required for public repositories")
+	flag.StringVar(&region, "region", "", "The AWS region")
+
+}
+
 func main() {
+	flag.Parse()
 
 	b := NewBuilder()
 
-	err := b.buildFromGithub("github.com/apanto/foo", "/dev/authz/github", "foo")
+	if repo == "" {
+		log.Fatal("GitHub repository name is required")
+	}
+
+	if region != "" {
+		b.SetRegion(region)
+	}
+
+	err := b.buildFromGithub(repo, credentials_ssm_key, cname)
 	if err != nil {
 		panic(err)
 	}
